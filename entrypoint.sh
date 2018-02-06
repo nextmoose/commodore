3d363e5cdf55117da8ffd643ab246a7aad3d553c
Emory Merryman <emory.merryman@gmail.com>
2018-02-06 11:43:48 +0000

    date

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,20 @@
 while [ ${#} -gt 0 ]
 do
     case ${1} in
+        --expiry)
+            export EXPIRY=$(date --date "${2}") &&
+                shift 2
+        ;;
         --project-name)
             export PROJECT_NAME="${2}" &&
                 shift 2
         ;;
     esac
 done &&
+    if [ ! -z "${EXPIRY}" ]
+    then
+        export EXPIRY=$(date --date "now + 1 month")
+    fi &&
     cleanup(){
         sudo --preserve-env docker-compose stop &&
             sudo --preserve-env docker-compose rm -fv
