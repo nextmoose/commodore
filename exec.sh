574e8af558efe32e4c6e7f2ae1ae2d4f59b837a3
Emory Merryman <emory.merryman@gmail.com>
2018-02-06 11:46:29 +0000

    DISPLAY

--- exec.sh
+++ exec.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 sudo \
+    --preserve-env \
     docker \
     run \
     --interactive \
     --tty \
     --rm \
+    --env DISPLAY \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
     --label expiry=$(($(date +%s)+60*60*24*7)) \
     docker:18.01.0-ce \
@@ -14,6 +16,7 @@ sudo \
         --interactive \
         --tty \
         --rm \
+        --env DISPLAY ]
         --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
         --label expiry=$(($(date +%s)+60*60*24*7)) \
         rebelplutonium/commodore:0.0.0
