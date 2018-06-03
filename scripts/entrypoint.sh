f54233e06ba55d9fdf1eb993b14003dfb0e1b598
Emory Merryman <emory.merryman@gmail.com>
2018-06-03 11:46:02 +0000

    reboot

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case ${1} in
+    up)
+        sh /opt/scripts/up.sh &&
+            exit 0
+    ;;
+    down)
+        sh /opt/scripts/down.sh &&
+            exit 0
+    ;;
+    *)
+        echo Unknown Option &&
+            echo ${0} &&
+            echo ${@} &&
+            exit 64
+    ;;
+esac
